c330926a63132900f30ffed2e4911b215aa53584
Lucberfer <lucberfer@gmail.com>
2024-10-23 01:30:12 +0200

    Add data

---
+++ miCalculadora/src/main/java/com/mycompany/micalculadora/MiCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.micalculadora;
+
+/**
+ *
+ * @author lucas
+ */
+public class MiCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
